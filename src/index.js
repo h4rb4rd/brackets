f188572f088396f10a6f6d82f2c794ae6264d95f
h4rb4rd <75742825+h4rb4rd@users.noreply.github.com>
2021-07-26 00:41:28 +0300

    it's done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketPairs = bracketsConfig.map((bracket) => {
+    return bracket[0] + "" + bracket[1];
+  });
+
+  let prevLength = str.length;
+
+  while (str.length) {
+    for (const pair of bracketPairs) {
+      str = str.replace(pair, "");
+    }
+    if (prevLength === str.length) {
+      return false;
+    }
+    prevLength = str.length;
+  }
+
+  return true;
 }
